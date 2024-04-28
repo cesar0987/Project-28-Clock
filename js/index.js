4043ffc049b886d571dcb1ed8d83d5a2d2e62181
cesar0987 <37241515+cesar0987@users.noreply.github.com>
2024-04-27 23:04:55 -0400

    Clock

---
+++ js/index.js
@@ -0,0 +1,40 @@
+const hour = document.getElementById("hour");
+const minutes = document.getElementById("minutes");
+const seconds = document.getElementById("seconds");
+function getSecondsSinceStartOfDay() {
+    return new Date().getSeconds() + 
+      new Date().getMinutes() * 60 + 
+      new Date().getHours() * 3600;
+  }
+  function getHourHandAngle(time) {
+    time %= 43200; // seconds in 12 hours
+    return ((360 * time / 43200) + 180) % 360;
+  }
+  
+  function getMinuteHandAngle(time) {
+    time %= 3600; // seconds in an hour
+    return ((6 * time / 60) + 180) % 360;
+  }
+  
+  function getSecondHandAngle(time) {
+    time %= 60; // seconds in a minute
+    return ((6 * time) + 180) % 360;
+  }
+  
+
+  
+  function getSecondsSinceStartOfDay() {
+    return new Date().getSeconds() + 
+      new Date().getMinutes() * 60 + 
+      new Date().getHours() * 3600;
+  }
+      
+  setInterval( function() {
+      var time = getSecondsSinceStartOfDay();
+      hour.style.transform = `rotate(${getHourHandAngle(time)}deg)`;
+      minutes.style.transform = `rotate(${getMinuteHandAngle(time)}deg)`;
+      seconds.style.transform = `rotate(${getSecondHandAngle(time)}deg)`;
+      console.log(time);
+      // your code here
+  }, 1000);
+  
